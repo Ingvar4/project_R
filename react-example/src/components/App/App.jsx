95f339de59af273cf29d79ba884f798e086160f4
Ingvar <jamessoer91@gmail.com>
2025-07-10 11:31:45 +0300

    start react project

---
+++ react-example/src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import { MyName } from '../MyName/MyName';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Привет Мир!</h1>
+        <p>Это первый проект на React</p>
+        <MyName />
+        
+      </div>
+    </>
+  )
+}
+
+export default App
