7c2f66a08f644453656f15c2a76449996254a04a
Ingvar4 <jamessoer91@gmail.com>
2026-04-02 14:17:29 +0300

    432

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,9 @@ import './App.css';
 
 
 function App() {
-  
-  const numbers = [1, 2, 3, 4, 5, 6];
-  const listItems = numbers.map((number) => <li key={number.toString()}>{number}</li>);
   return (
     <>
-      <div>
-        <h1>Заголовок 1</h1>
-        <p>это параграф</p>
-        <ul>{listItems}</ul>
-      </div>
+      <h1>Заголовок</h1>
     </>
   )
 }
