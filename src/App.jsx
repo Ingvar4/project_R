e61ff475118f9031846a004ae3ad4dc49c61be31
Ingvar4 <jamessoer91@gmail.com>
2026-04-06 10:18:15 +0300

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
- import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import './App.css';
+
+import { RefComponents } from "./components/RefComponent";
 
 function App() {
   
@@ -31,6 +33,7 @@ function App() {
 
       return(
         <>
+          <RefComponents />
           <ul>
             {data.map((item) => (
               <li key={item.id}>{item.title}</li>
